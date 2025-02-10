3076b051fb790e47f951b78d377fa3bc7595d076
建明 武 <wjmwjmwb@yeah.net>
2025-02-10 17:35:00 +0800

    小小优化

--- backend/retriever/vector_db.py
+++ backend/retriever/vector_db.py
@@ -5,8 +5,6 @@
 # @Time: 2024/9/1 19:29
 
 from tqdm import tqdm
-import Stemmer
-
 from langchain_chroma.vectorstores import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
@@ -16,37 +14,41 @@ from langchain_huggingface import HuggingFaceEmbeddings
 
 
 class VectorDB:
-    def __init__(self, db_id, db_path=None, embed_func=None):
-        self.db_id = db_id
-        if db_id == "Chroma":
-            self.db = Chroma(persist_directory=db_path, embedding_function=embed_func)
-            if isinstance(self.db.embeddings, HuggingFaceEmbeddings) \
-                    and self.db.embeddings.client.tokenizer.pad_token is None:
-                self.db.embeddings.client.tokenizer.pad_token = self.db.embeddings.client.tokenizer.eos_token
-        elif db_id == "BM25":
-            self.db = embed_func
+    """向量数据库,用于向量检索"""
+    
+    def __init__(self, db_path, embed_func):
+        """初始化向量数据库
+        
+        Args:
+            db_path: 数据库路径
+            embed_func: 嵌入函数
+        """
+        self.db = Chroma(persist_directory=db_path, embedding_function=embed_func)
+        if isinstance(self.db.embeddings, HuggingFaceEmbeddings) \
+                and self.db.embeddings.client.tokenizer.pad_token is None:
+            self.db.embeddings.client.tokenizer.pad_token = self.db.embeddings.client.tokenizer.eos_token
 
-    def load_vector(self, docs, batch_size=64, top_k=5):
-        if self.db_id == "BM25":
-            # We can pass in the index, docstore, or list of nodes to create the retriever
-            self.db = self.db.from_defaults(
-                nodes=docs,
-                similarity_top_k=top_k,
-                # Optional: We can pass in the stemmer and set the language for stopwords
-                # This is important for removing stopwords and stemming the query + text
-                # The default is english for both
-                stemmer=Stemmer.Stemmer("english"),
-                language="english",
-            )
-
-        else:
-            texts = [doc.page_content for doc in docs]
-            metadatas = [doc.metadata for doc in docs]
-            ids = [str(i) for i in range(len(docs))]
-
-            self.batch_upsert(texts, metadatas, ids, batch_size)
+    def load_vector(self, docs, batch_size=64):
+        """加载文档向量到数据库
+        
+        Args:
+            docs: 文档列表
+            batch_size: 批处理大小
+        """
+        texts = [doc.page_content for doc in docs]
+        metadatas = [doc.metadata for doc in docs]
+        ids = [str(i) for i in range(len(docs))]
+        self.batch_upsert(texts, metadatas, ids, batch_size)
 
     def batch_upsert(self, texts, metadatas, ids, batch_size):
+        """批量插入/更新文档
+        
+        Args:
+            texts: 文本列表
+            metadatas: 元数据列表
+            ids: ID列表
+            batch_size: 批处理大小
+        """
         for i in tqdm(range(0, len(texts), batch_size)):
             end = min(i + batch_size, len(texts))
             current_texts = texts[i:end]
