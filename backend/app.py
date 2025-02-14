e799c05a916119a198019a102f97c66924c92286
Beliefuture <3066443504@qq.com>
2025-02-15 01:25:59 +0800

    update configuration

--- backend/app.py
+++ backend/app.py
@@ -4,4 +4,4 @@ app = create_app(config_name="DEVELOPMENT")
 app.app_context().push()
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=30006)
+    app.run(debug=True, host="0.0.0.0", port=5130)
