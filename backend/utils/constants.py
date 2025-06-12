50b8e093181c65467defaa67a5dfe6d3e53246a8
BoyuXiao <2092327901@qq.com>
2025-06-12 22:22:40 +0800

    fix the bug about rule_rewrite

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -17,11 +17,15 @@ DIALECT_LIST = ["pg", "mysql", "oracle"]
 DIALECT_LIST_RULE = ["athena", "bigquery", "clickhouse", "databricks", "doris", "drill", "druid",
                      "duckdb", "dune", "hive", "materialize", "mysql", "oracle", "postgres",
                      "presto", "prql", "redshift", "risingwave", "snowflake", "spark", "spark2",
-                     "sqlite", "starrocks", "tableau", "teradata", "trino", "tsql", "postgresql"]
+                     "sqlite", "starrocks", "tableau", "teradata", "trino", "tsql"]
 DIALECT_MAP = {
     'pg': 'PostgreSQL 14.7',
     'mysql': "MySQL 8.4",
     'oracle': "Oracle 11g"
+}
+
+DIALECT_ABBREVIATIONS = {
+    'pg': 'postgres'
 }
 
 ORACLE_COMMAND_OPEN = False
